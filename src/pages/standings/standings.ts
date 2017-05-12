3d73846f47387fb8f532bbb9c26142c4591a38b8
envy7 <noel.varghese.22@gmail.com>
2017-05-12 19:50:03 +0530

    virtual scrolling and segments added to standings page

--- src/pages/standings/standings.ts
+++ src/pages/standings/standings.ts
@@ -3,6 +3,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 import { EliteApi } from '../../shared/shared';
 import _ from 'lodash';
+
 @IonicPage()
 @Component({
   selector: 'page-standings',
@@ -12,6 +13,7 @@ export class StandingsPage {
   standings: any[];
   team: any;
   allStandings: any[];
+  divisionFilter:string = "division";
   constructor(public navCtrl: NavController, 
               public navParams: NavParams,
               private eliteApi: EliteApi) {
@@ -22,15 +24,36 @@ export class StandingsPage {
     let tourneyData = this.eliteApi.getCurrentTourney();
     this.standings = tourneyData.standings;
 
-    this.allStandings = 
-      _.chain(this.standings)
-       .groupBy('division')
-       .toPairs()
-       .map(item => _.zipObject(['divisionName', 'divisionStandings'], item))
-      .value();
+    // this.allStandings = 
+    //   _.chain(this.standings)
+    //    .groupBy('division')
+    //    .toPairs()
+    //    .map(item => _.zipObject(['divisionName', 'divisionStandings'], item))
+    //   .value();
 
     console.log('standings', this.standings);
-    console.log('division Standings', this.allStandings);  
+    // console.log('division Standings', this.allStandings);  
+    this.allStandings = tourneyData.standings;
+
+    this.filterDivision();
   }
+
+  filterDivision() {
+    if(this.divisionFilter === 'all') {
+      this.standings = this.allStandings;
+    }
+    else {
+      this.standings = _.filter(this.allStandings, s => s.division === this.team.division);
+    }
+  }
+
+  getHeader(record, recordIndex, records){
+    if (recordIndex === 0 || record.division !== records[recordIndex-1].division) {
+      return record.division;
+    }
+    return null;  
+  }
+
+
 
 }
