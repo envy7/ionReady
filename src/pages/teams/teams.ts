ca12c120c06a30a1badcbe83baefe04027dbd950
envy7 <noel.varghese.22@gmail.com>
2017-05-12 19:54:35 +0530

    search added to teams page

--- src/pages/teams/teams.ts
+++ src/pages/teams/teams.ts
@@ -14,6 +14,7 @@ export class TeamsPage {
  private allTeams: any;
  private allTeamDivisions: any; 
  teams = [];
+ queryText: string;
 
   constructor(public navCtrl: NavController, 
               public navParams: NavParams,
@@ -46,9 +47,23 @@ export class TeamsPage {
         console.log('division teams', this.teams);
         loader.dismiss();    
       }) 
-    });
+    });  
+  }
 
-    
+  updateTeams() {
+    let queryTextLower = this.queryText.toLowerCase();
+    let filteredTeams = [];
+    _.forEach(this.allTeamDivisions, td => {
+        let teams = _.filter(td.divisionTeams, t => (<any>t).name.toLowerCase().includes(queryTextLower));
+        if(teams.length) {
+          filteredTeams.push({
+            divisionName: td.divisionName,
+            divisionTeams: teams
+          })
+        }
+
+     this.teams = filteredTeams;   
+    })
   }
 
 }
